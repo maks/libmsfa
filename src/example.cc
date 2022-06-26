d4d178e53d1af4e08eb4417e30f0cc644d4a56a3
Maksim Lin <maks@manichord.com>
2022-06-26 17:32:37 +1000

    create simple API with header for libmsfa

--- src/example.cc
+++ src/example.cc
@@ -17,22 +17,12 @@
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
+#include <unistd.h>
 
-#include "synth.h"
-#include "synth_unit.h"
-#include "module.h"
-#include "freqlut.h"
-#include "sin.h"
-#include "sawtooth.h"
+#include "../include/libmsfa.h"
 
 //for initial onlytesting
 #include "patch.h"
-
-
-#define MINIAUDIO_IMPLEMENTATION
-#include "../include/miniaudio.h"
-
-double sample_rate = 44100.0;
 
 char epiano[] = {
   95, 29, 20, 50, 99, 95, 0, 0, 41, 0, 19, 0, 115, 24, 79, 2, 0, 95, 20, 20,
@@ -44,20 +34,7 @@ char epiano[] = {
   32
 };
 
-RingBuffer *ring_buffer;
-SynthUnit *synth_unit;
-
-
-void data_callback(ma_device *pDevice, void *pOutput, const void *pInput, ma_uint32 frameCount)
-{
-  //std::cout << "callback!" << frameCount << std::endl;
-
-  // Use MSFA frames
-  synth_unit->GetSamples(frameCount, (int16_t *)pOutput);
-
-  (void)pInput;
-}
-
+// read a syx patch bank file from filename path and load it into the synth engine
 int load(const char *filename) {
   uint8_t syx_data[4104];
   std::ifstream fp_in;
@@ -71,7 +48,7 @@ int load(const char *filename) {
     std::cerr << "error reading file" << std::endl;
     return 1;
   }
-  ring_buffer->Write(syx_data, 4104);
+  sendMidi(syx_data, 4104);
   return 0;
 }
 
@@ -79,55 +56,28 @@ int main(int argc, char **argv)
 {
   printf("libmsfa sample...\n");
 
-  ma_result result;
-  ma_device_config deviceConfig;
-  ma_device device;
-  // ma_format format;
-  // ma_uint32 channels;
-  // ma_uint32 sampleRate;
-
-  // Init MSFA engine
-  SynthUnit::Init(sample_rate);
-  ring_buffer = new RingBuffer();
-  synth_unit = new SynthUnit(ring_buffer);
-
+  if (initEngine() != MSFA_SUCCESS) {
+    printf("Failed to initialise MSFA engine\n");
+    return 1;
+  }
+ 
   const char *fn = "../data/rom1a.syx";
   load(fn);
-
-
-  deviceConfig = ma_device_config_init(ma_device_type_playback);
-  deviceConfig.playback.format = ma_format_s16;
-  deviceConfig.playback.channels = 1;
-  deviceConfig.sampleRate = ma_standard_sample_rate_44100;
-  deviceConfig.dataCallback = data_callback;
-
-  if (ma_device_init(NULL, &deviceConfig, &device) != MA_SUCCESS)
-  {
-    printf("Failed to open playback device.\n");
-    return -3;
-  }
-
-  if (ma_device_start(&device) != MA_SUCCESS)
-  {
-    printf("Failed to start playback device.\n");
-    ma_device_uninit(&device);
-    return -4;
-  }
 
   uint8_t midiNoteNumber = 0x4c;
   uint8_t midiNoteDown[] = {0x90, midiNoteNumber, 0x57};
   uint8_t midiNoteUp[] = {0x90, midiNoteNumber, 0x00};
   
   sleep(1);
-  ring_buffer->Write(midiNoteDown, 3);
+  sendMidi(midiNoteDown, 3);
   sleep(3);
  
-  ring_buffer->Write(midiNoteUp, 3);
+  sendMidi(midiNoteUp, 3);
 
   printf("Press Enter to quit...");
   getchar();
 
-  ma_device_uninit(&device);
+  shutdownEngine();
 
   return 0;
 }
