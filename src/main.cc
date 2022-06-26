ef66eeb045c9c533d205c725269462bf5619555b
Maksim Lin <maks@manichord.com>
2022-06-26 17:32:37 +1000

    add miniaudio with basic working file playback

--- src/main.cc
+++ src/main.cc
@@ -1,12 +1,12 @@
 /*
  * Copyright 2012 Google Inc.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -24,6 +24,29 @@
 #include "sin.h"
 #include "sawtooth.h"
 
-int main(int argc, char **argv) {
+#define MINIAUDIO_IMPLEMENTATION
+#include "../include/miniaudio.h"
+
+int main(int argc, char **argv)
+{
   printf("libmsfa sample...\n");
+
+  ma_result result;
+  ma_engine engine;
+
+  result = ma_engine_init(NULL, &engine);
+  if (result != MA_SUCCESS)
+  {
+    printf("Failed to initialize audio engine.");
+    return -1;
+  }
+
+  ma_engine_play_sound(&engine, argv[1], NULL);
+
+  printf("Press Enter to quit...");
+  getchar();
+
+  ma_engine_uninit(&engine);
+
+  return 0;
 }
